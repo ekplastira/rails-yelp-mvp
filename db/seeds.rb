029c66c0f657c73a44cbe03ff90385a94cacef6f
Katerina Plastira <ekplastira@gmail.com>
2020-11-25 16:35:37 +0100

    restaurant yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "starting seed..."
+Restaurant.destroy_all
+puts "creating restaurants"
+100.times do
+  restaurant = Restaurant.create!(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.full_address,
+  phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+  category: CATEGORY.sample
+  )
+  puts "#{restaurant.name} created"
+end
+
